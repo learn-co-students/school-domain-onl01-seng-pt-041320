8e1367aa092cf394c117e1cbd737d306719dc86b
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-28 02:22:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
+require 'pry'
 # code here!
+class School
+  attr_accessor :roster
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+  #add students
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade] << name
+    if @roster[grade].length > 1
+      roster = @roster[grade]
+      roster << name
+    end
+  end
+end

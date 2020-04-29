9c3041df4597b9c1c70e97f301482534a8a3f8f2
Andrew Alteri <andrewsalteri@gmail.com>
2020-04-29 13:09:51 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_reader :name
+    attr_accessor :roster
+    
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade] != nil
+            @roster[grade] << name
+        else
+        @roster[grade] = []
+        @roster[grade] << name     
+        end 
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+       @roster.each {|grade, name| name.sort!} 
+    end
+end
+

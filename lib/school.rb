f01f0505fbcfa0056662a3346a1609aebb67dee3
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-30 16:06:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+# code here
+class School
+    def initialize(new_school)
+      @new_school = new_school
+      @roster = {}
+   end
+
+   def roster
+     @roster
+   end
+
+  def add_student(name, grade)
+     @add_student = name
+     @grade = grade
+    if !@roster.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+   def grade(grade)
+     @roster[grade]
+   end
+
+   def sort
+      @roster.each do |grade, name|
+      @roster[grade] = name.sort
+      end
+   end
+
+end

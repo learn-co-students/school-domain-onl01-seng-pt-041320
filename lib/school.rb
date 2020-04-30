06f5a21cab28832c203473f3fea1af3f2bd810b8
Joann Brady <jbrady11934@optimum.net>
2020-04-30 16:34:58 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+def initialize (school_name)
+@school_name = school_name
+@roster = {}
+end
+
+hash["new_key"] = []
+hash["new_key"] << "new_value_for_value_array"
+
+def school_name
+    @school_name
+end
+
+def roster= {}
+@roster = {}
+end
+
+def add_student(student_name, grade)
+@student_name = student_name
+@grade = grade
+end
+
+def grade(grade)
+    grade
+
+
+
+end

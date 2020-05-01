8f268cd7c08025e567b2f7f6f783ddf2f0ae8bb1
Joann Brady <jbrady11934@optimum.net>
2020-05-01 11:16:03 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,33 @@
 # code here!
 class School
-
-def initialize (school_name)
-@school_name = school_name
-@roster = {}
-end
-
-hash["new_key"] = []
-hash["new_key"] << "new_value_for_value_array"
-
-def school_name
-    @school_name
-end
-
-def roster= {}
-@roster = {}
-end
-
-def add_student(student_name, grade)
-@student_name = student_name
-@grade = grade
-end
-
-def grade(grade)
-    grade
-
-
-
-end
+    attr_reader :school_name, :roster
+    
+    def initialize(school_name)
+    @school_name = school_name
+    @roster ={}
+    end
+    
+    def roster
+    @roster
+    end
+    
+    def add_student(student_name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << student_name
+        else  
+    @roster[grade] = [student_name]
+    end
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        self.roster.each do |grade, students|
+            students.sort!
+        end
+    end
+    
+    end
+    

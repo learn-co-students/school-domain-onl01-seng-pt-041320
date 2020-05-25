bb01ad058aa0619bf00f6d15c0c11488575ba2b4
chica25 <patricia@patriciaco.com>
2020-05-24 21:41:53 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,44 +1,81 @@
-class School
-attr_accessor :roster
+# class School
+# attr_accessor :roster
 
-def initialize(name)
-  @name = name
-  @roster = {}
-end
-
-# def add_student(name, grade)
-    # roster => {}
-  #   if @roster[grade]
-  #   @roster[grade] << name
-  # else
-  #   @roster[grade] = []
-  #   @roster[grade] << name
-  #   end
-  # end
-
-# ----- 2nd option with the ternary operator -----
-# def add_student(name, grade)
-# @roster[grade] ? @roster[grade].<<[name] : (@roster[grade] = []; @roster[grade]<< name)
+# def initialize(name)
+#   @name = name
+#   @roster = {}
 # end
 
-# ----- third option -------
-def add_student(name, grade)
-  @roster[grade] ||= []
-  @roster[grade] << name
-end
+# # def add_student(name, grade)
+#     # roster => {}
+#   #   if @roster[grade]
+#   #   @roster[grade] << name
+#   # else
+#   #   @roster[grade] = []
+#   #   @roster[grade] << name
+#   #   end
+#   # end
 
-def grade(grade_number)
-  @roster[grade_number]
+# # ----- 2nd option with the ternary operator -----
+# # def add_student(name, grade)
+# # @roster[grade] ? @roster[grade].<<[name] : (@roster[grade] = []; @roster[grade]<< name)
+# # end
+
+# # ----- third option -------
+# def add_student(name, grade)
+#   @roster[grade] ||= []
+#   @roster[grade] << name
+# end
+
+# def grade(grade_number)
+#   @roster[grade_number]
+#   end
+
+# def sort
+#   # iterate over roster hash
+#   # for each key (grade) in roster hash, sort the value(student names array) alphabetically
+#   # return sorted array
+#   sorted = {}
+#     @roster.each do |grade, names_array|
+#       sorted[grade] = names_array.sort
+#     end
+#     sorted
+#   end
+# end
+
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {} 
+  end
+#add_student notes ---
+# school.add_student("Zach Morris", 9)
+# school.roster
+# => {9 => ["Zach Morris"]}
+  def add_student(name, grade)
+    #if roster.include?(grade)
+    #if roster.member?(grade)
+    if roster.has_key?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
   end
 
-def sort
-  # iterate over roster hash
-  # for each key (grade) in roster hash, sort the value(student names array) alphabetically
-  # return sorted array
-  sorted = {}
-    @roster.each do |grade, names_array|
-      sorted[grade] = names_array.sort
+  def grade(s_grade)
+    @roster[s_grade]
+  end
+
+  def sort
+      sorted = {}
+      roster.each do |grade, name|
+        sorted[grade] = name.sort
     end
     sorted
   end
+
 end
+# school = School.new("Bayside High School")
+# school.roster
